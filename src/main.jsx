04920dd42cf54c5ca671813a6341a03921ec2898
FredrickOguya <onyangofredrickoguya@gmail.com>
2024-07-30 17:10:02 +0300

    first push

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('main')).render(
+  <React.StrictMode>
+    <div>hello world</div>
+  </React.StrictMode>,
+);
+
